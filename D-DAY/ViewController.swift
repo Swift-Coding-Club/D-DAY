e6704b26204b66901b90de05937e78ba316f8b50
Minji Kang <aldalddl2007@gmail.com>
2023-01-14 14:14:15 +0900

    Initial Commit

---
+++ D-DAY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  D-DAY
+//
+//  Created by MinJi Kang on 2023/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ D-DAYUITests/D_DAYUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  D_DAYUITestsLaunchTests.swift
+//  D-DAYUITests
+//
+//  Created by MinJi Kang on 2023/01/14.
+//
+
+import XCTest
+
+final class D_DAYUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
